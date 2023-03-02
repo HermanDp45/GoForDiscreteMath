968d7a9722abda2b9567b27ccaada74402d1adf3
HermannDp <HermannDp@edelkin.ru>
2023-03-02 16:39:14 +0300

    0.2

--- Go/TEST/Test.go
+++ Go/TEST/Test.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-    //"errors"
-    "fmt"
-    //"unicode"
-    "strconv"
-
+	//"errors"
+	"fmt"
+	//"unicode"
+	"strconv"
 )
 
 func main() {
-    //var users []string
+	//var users []string
 
-    //x := func(fn func(i int) int, i int) func(int) int { return fn }(func(i int) int { return i + 1 }, 5)
-    //fmt.Print(string('0'));
-    x, _ := strconv.ParseUint("123", 10, 64)
-    fmt.Print(x);
+	x, _ := strconv.ParseUint("123", 10, 64)
+	fmt.Print(x)
 }
